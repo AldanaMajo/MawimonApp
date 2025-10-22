e686bcd8b37673c4c08bf1c3a31122f1e0c8dd52
AldanaMajo <majoaldana178@gmail.com>
2025-10-22 00:46:19 -0600

    menu inferior

--- componentes/Layout.js
+++ componentes/Layout.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Text,StatusBar, View } from 'react-native';
-
+import { StyleSheet, Text,StatusBar, View, TouchableOpacity  } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context'
 
+import Icon from 'react-native-vector-icons/Feather'; 
+const navigation = useNavigation();
 // Header
 const Header = ({label}) => (
   <View style={styles.header}>
@@ -14,20 +16,30 @@ const Body = ({ children }) => (
     {children}
   </View>
 );
+const FooterButton = ({ icon, label, onPress }) => (
+  <TouchableOpacity style={styles.footerButton} onPress={onPress}>
+    <Icon name={icon} size={24} color="#FFA500" />
+    <Text style={styles.footerLabel}>{label}</Text>
+  </TouchableOpacity>
+);
 
 // Footer
 const Footer = () => (
   <View style={styles.footer}>
-    <Text style={styles.footerText}></Text>
+     <FooterButton icon="home" label="Inicio" onPress={() => navigation.navigate('Inicio')} />
+      <FooterButton icon="search" label="Buscar" />
+      <FooterButton icon="heart" label="Favoritos" />
+      <FooterButton icon="settings" label="Ajustes" />
   </View>
 );
+
 
 // Layout
 export default function Layout({header,children}) {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
+        <StatusBar barStyle="auto"  />
         <Header label={header} />
         <Body>
          {children}
@@ -42,19 +54,17 @@ const styles = StyleSheet.create({
  
   container: {
     flex: 1, 
-    backgroundColor: '#f5f5f5', 
   },
 
   header: {
-    height: 60, 
-    backgroundColor: '#740073', 
+    height: 35,
     justifyContent: 'center', 
-    alignItems: 'center', 
+    alignItems: 'center',   
   },
 
   headerText: {
-    color: 'white',
-    fontSize: 20, 
+    color: 'black',
+    fontSize: 24, 
     fontWeight: 'bold', 
   },
   
@@ -66,13 +76,19 @@ const styles = StyleSheet.create({
   },
  
   footer: {
-    height: 50, 
-    backgroundColor: '#4a0045', 
-    justifyContent: 'center',
+    height: 80, 
+    backgroundColor: '#3384cc', 
     alignItems: 'center', 
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
-  footerText:{
-    color: 'white',
-   
+  footerButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerLabel: {
+    color: '',
+    fontSize: 14,
+    marginTop: 4,
   },
 });
